3f2f9465f03818854c86fc979f932c7b7a6bd8aa
Patryk Pastuszka <nov11157@gmail.com>
2025-09-07 13:44:49 +0200

    Added tooltips Added movement calculation example to WeaponEdgeTypeDrawer.cs

--- Data/WeaponEdgeClassifierParams.cs
+++ Data/WeaponEdgeClassifierParams.cs
@@ -11,18 +11,40 @@ namespace WeaponsMath.Data
         public const int MAX_COLLECTED = 1024;
         public const int MAX_DEPTH = 128;
         public const int MAX_NEIGHBORS = 128;
-        
-        [Range(0, MAX_DEPTH)] public int depth; // n-depth BFS (best: 1..32)
-        [Range(0, MAX_NEIGHBORS)] public int maxNeighbors; // cap on neighbors to consider (0 = no cap)
-        [Range(0, 1f)] public float splitLow; // <= => Blunt (0..1)
-        [Range(0, 1f)] public float splitHigh; // >= => Spike (0..1)
-        [Range(0, 1f)] public float depthDecay; // weight multiplier per depth level (e.g. 0.7)
 
-        [Range(0, 2f)]
+        [Range(0, MAX_DEPTH)]
+        [Tooltip("Depth of scanning, recommended values: 1..32, but keep it low to avoid performance issues")]
+        public int depth; // n-depth BFS (best: 1..32)
+
+        [Range(0, MAX_NEIGHBORS)]
+        [Tooltip(
+            "Safety cap for neighbors to scan, recommended values: 8..128. Zero is disabled (not recommended)")]
+        public int maxNeighbors; // cap on neighbors to consider (0 = no cap)
+
+        [Range(0, 1f)] [Tooltip("Values under this one will be considered blunt, above are blades or spikes")]
+        public float splitLow; // <= => Blunt (0..1)
+
+        [Range(0, 1f)]
+        [Tooltip(
+            "Values above this one will be considered spikes, when lower then it will be considered blades (only if higher than " +
+            nameof(splitLow) + ")")]
+        public float splitHigh; // >= => Spike (0..1)
+
+        [Range(0, 1f)]
+        [Tooltip("Weight multiplier per depth level - reduces weight of vertices that are not directly connected")]
+        public float depthDecay; // weight multiplier per depth level (e.g. 0.7)
+
+        [Range(0, 1f)] [Tooltip("0 = disabled, when above it causes weight to decrease with distance")]
         public float distanceWeightPower; // 0 = disabled, >0 to scale inverse-distance weight (e.g. 1.0)
 
+        [Tooltip(
+            "Minimum distance between vertices to avoid division by zero, recommended values 1e-6..1e-12, use lower values for more precision in some meshes")]
         public float minSqrDistance; // avoid divis by zero
-        [Range(1, MAX_COLLECTED)] public int maxCollected; // safety cap
+
+        [Range(1, MAX_COLLECTED)]
+        [Tooltip(
+            "Limit neighbours collection mechanism, recommended values: 16..1024, keep it low to avoid performance issues")]
+        public int maxCollected; // safety cap
 
         public static WeaponEdgeClassifierParams Default => new()
         {
--- WeaponEdgeTypeDrawer.cs
+++ WeaponEdgeTypeDrawer.cs
@@ -1,5 +1,6 @@
 ﻿// Example MonoBehaviour to compute per-vertex continuous score and visualize in Editor (Gizmos)
 
+using Unity.Mathematics;
 using UnityEngine;
 using WeaponsMath.Data;
 using WeaponsMath.Enums;
@@ -8,11 +9,20 @@ namespace WeaponsMath
 {
     public sealed class WeaponEdgeTypeDrawer : MonoBehaviour
     {
-        public MeshFilter meshFilter;
+        [Header("Classification")] public MeshFilter meshFilter;
         public WeaponEdgeClassifierParams parameters = WeaponEdgeClassifierParams.Default;
 
+        [Header("Visualization")] [Tooltip("Multiplier for normal length, used to make drawing easier to read")]
         public float normalLength = 0.01f;
+
+        [Tooltip("Multiplier for sphere radius, used to make drawing easier to read")]
         public float baseSphereRadius = 0.002f;
+
+        [Header("Movement affection")] [Tooltip("Should drawn values be affected by movement vector?")]
+        public bool enableMovementAffection;
+
+        [Tooltip("Movement vector, used to affect drawn values")] public Vector3 movementVector = Vector3.forward;
+        [Tooltip("Movement velocity, used to affect drawn values")] public float velocity = 1f;
 
         private float[] scores;
         private WeaponEdgeType[] types;
@@ -37,6 +47,9 @@ namespace WeaponsMath
             parameters.splitHigh = Mathf.Clamp01(parameters.splitHigh);
             if (parameters.splitLow >= parameters.splitHigh)
                 parameters.splitHigh = Mathf.Min(1f, parameters.splitLow + 0.01f);
+
+            // normalize visualization vector
+            movementVector.Normalize();
         }
 
         private void OnDrawGizmosSelected()
@@ -47,6 +60,14 @@ namespace WeaponsMath
             if (mesh == null) return;
 
             Transform meshTransform = meshFilter.transform;
+            Vector3 position = meshTransform.position;
+
+            // Remap visualization vector to mesh
+            Vector3 remappedVisualizationVector = meshTransform.TransformDirection(movementVector);
+
+            // Draw remapped visualization vector from mesh center
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(position, position + remappedVisualizationVector * 0.5f);
 
             Vector3[] verts = mesh.vertices;
             for (int i = 0; i < verts.Length; ++i)
@@ -65,10 +86,30 @@ namespace WeaponsMath
                 }
 
                 Gizmos.DrawSphere(vertexWorldPosition, baseSphereRadius * (1f + score));
-
-                // Draw normal
                 Vector3 normal = meshTransform.TransformDirection(mesh.normals[i]);
-                Gizmos.DrawLine(vertexWorldPosition, vertexWorldPosition + normal * normalLength);
+
+                // Draw normal based on movement affection
+                // This considers movement vector and velocity to affect normal length
+                // based on E = 1/2 * m * v^2 formula while keeping normal angle affection
+                // quite high to ensure that quadratic effect won't be too strong on non-related
+                // normals what could yield terrible results.
+                if (enableMovementAffection)
+                {
+                    // Compute dot product of normal and visualization vector
+                    float dotProduct = math.dot(normal, remappedVisualizationVector);
+                    dotProduct = math.max(dotProduct, 0); // Prevent angles greater than 90 degrees
+
+                    // Increase strength
+                    dotProduct = math.pow(dotProduct, 4);
+
+                    // Compute new normal and draw it
+                    Vector3 fixedNormal = normal * (normalLength * dotProduct * math.pow(velocity, 2));
+                    Gizmos.DrawLine(vertexWorldPosition, vertexWorldPosition + fixedNormal);
+                }
+                else
+                {
+                    Gizmos.DrawLine(vertexWorldPosition, vertexWorldPosition + normal * normalLength);
+                }
             }
         }
     }
